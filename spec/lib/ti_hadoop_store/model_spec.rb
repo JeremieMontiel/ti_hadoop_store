b2b9ed9232f76c97cd499061c0b3409f58b0e7c3
Emmanuel Bastien <os@ebastien.name>
2015-04-16 22:27:02 +0200

    Setup rspec dummy application.

--- spec/lib/ti_hadoop_store/model_spec.rb
+++ spec/lib/ti_hadoop_store/model_spec.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-require 'spec_helper'
+require 'rails_helper'
 require 'ti_hadoop_store'
 
 describe TiHadoopStore::Model do
---
+++ spec/rails_helper.rb
@@ -0,0 +1,4 @@
+ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
+require File.expand_path("../../spec/dummy/config/environment", __FILE__)
+require 'rspec/rails'
